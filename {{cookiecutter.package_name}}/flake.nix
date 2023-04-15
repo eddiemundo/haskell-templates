a0fc90156f1ef7a2ece3b0facb6582d3d5da135d
Jonathan Shen <document_done@hotmail.com>
2023-04-15 02:19:50 -0400

    update

--- {{cookiecutter.package_name}}/flake.nix
+++ {{cookiecutter.package_name}}/flake.nix
@@ -1,31 +1,41 @@
 {
-  description = "A very basic flake for {{cookiecutter.package_name}}";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/6083863f4ad49c59a74ed5496081727e23ccd754";
-    haskell-nix.url = "github:input-output-hk/haskell.nix";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, nixpkgs, haskell-nix, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        haskell-compiler-name = "ghc8104";
-        haskell-nix-pkgs = haskell-nix.legacyPackages.${system};
-        pkgs = import nixpkgs { inherit system; };
-        project = haskell-nix-pkgs.haskell-nix.cabalProject' {
-          src = ./.;
-          compiler-nix-name = haskell-compiler-name;
-        };
-     in {
-       # Built by `nix build .`
-       defaultPackage = project.hsPkgs.{{cookiecutter.package_name}}.components.exes.{{cookiecutter.package_name}};
-       
-       # This is used by `nix develop .` to open a shell for use with
-       devShell = project.shellFor {
-         buildInputs = with pkgs; [
-           cabal-install
-           haskell.packages."${haskell-compiler-name}".haskell-language-server
-         ];
-       };
-     }
-   );
+  description = "A very basic flake {{cookiecutter.package_name}}";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          {{cookiecutter.package_name}}Project =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc927";
+              # This is used by `nix develop .` to open a shell for use with
+              # `cabal`, `hlint` and `haskell-language-server`
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+                haskell-language-server = {};
+              };
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
+              # This adds `js-unknown-ghcjs-cabal` to the shell.
+              # shell.crossPlatforms = p: [p.ghcjs];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.{{cookiecutter.package_name}}Project.flake {
+        # This adds support for `nix build .#js-unknown-ghcjs:{{cookiecutter.package_name}}:exe:{{cookiecutter.package_name}}`
+        # crossPlatforms = p: [p.ghcjs];
+      };
+    in flake // {
+      # Built by `nix build .`
+      packages.default = flake.packages."{{cookiecutter.package_name}}:exe:{{cookiecutter.package_name}}";
+    });
 }
