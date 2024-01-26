5bb9a6c0cd56e38d57475d6f37e4a587630c0f7d
eddiemundo <jonathan@mlabs.com>
2024-01-25 23:51:48 -0500

    use flake-utils actually since the other method doesn't set up devShell properly

--- {{cookiecutter.package_name}}/flake.nix
+++ {{cookiecutter.package_name}}/flake.nix
@@ -10,35 +10,35 @@
         "x86_64-linux" 
         "x86_64-darwin" 
       ];
-      perSystem = nixpkgs.lib.genAttrs systems;
-      overlays = [
-        haskell-nix.overlay
-      ];
-      pkgs = perSystem (system:
-        import nixpkgs {
-          inherit system overlays;
-          inherit (haskell-nix) config;
-        }
+    in
+      flake-utils.lib.eachSystem systems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system overlays;
+            inherit (haskell-nix) config;
+          };
+          overlays = [
+            haskell-nix.overlay
+            (final: prev: {
+              {{cookiecutter.package_name}} = final.haskell-nix.project' {
+                compiler-nix-name = "ghc964";
+                src = ./.;
+                shell.tools = {
+                  cabal = {};
+                  hlint = {};
+                  fourmolu = {};
+                  haskell-language-server = { src = prev.haskell-nix.sources."hls-2.6"; };
+                };
+                shell.buildInputs = with pkgs; [
+                ];
+                inputMap = {};
+              };
+            })
+          ];
+          flake = pkgs.{{cookiecutter.package_name}}.flake {};
+        in
+          flake // {
+            packages.default = flake.packages."{{cookiecutter.package_name}}:exe:{{cookiecutter.package_name}}";
+          }
       );
-      project = perSystem (system: pkgs.${system}.haskell-nix.project {
-        compiler-nix-name = "ghc964";
-        src = ./.;
-        shell.tools = {
-          cabal = {};
-          hlint = {};
-          fourmolu = {};
-          haskell-language-server = { src = pkgs.haskell-nix.sources."hls-2.4"; };
-        };
-        # Non-Haskell shell tools go here
-        shell.buildInputs = with pkgs; [
-          nixpkgs-fmt
-        ];
-      });
-      flake = perSystem (system: project.${system}.flake {});
-    in flake // {
-      packages = perSystem (system: {
-        {{cookiecutter.package_name}} = flake.${system}.packages."{{cookiecutter.package_name}}:exe:{{cookiecutter.package_name}}";
-        default = self.packages.${system}.{{cookiecutter.package_name}};
-      });
-    }
 }
